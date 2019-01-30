c783de509b85c6647870a0c0c46e173ef3a37a11
louisjc <louisjc@users.noreply.github.com>
2019-01-30 11:30:20 -0600

    remove react-ga

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
 import 'bootstrap/dist/css/bootstrap-reboot.min.css'
 import './index.css'
 import App from './App'
-
-ReactGA.initialize('UA-77842893-1')
-ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(<App />, document.getElementById('root'))
