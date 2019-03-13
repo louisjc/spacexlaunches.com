4a75995cc5240e241929ab361fc55e247d57836e
louisjc <louisjc@users.noreply.github.com>
2019-03-13 15:13:52 -0500

    fix svg

--- src/scenes/PastLaunches/Destination.tsx
+++ src/scenes/PastLaunches/Destination.tsx
@@ -3,7 +3,7 @@ import DestinationSVG from './destinations.svg'
 
 const Destination: FunctionComponent<{ orbit: string }> = ({ orbit }) => (
   <span className={`destination ${orbit}`} title={orbit}>
-    <svg viewBox="30 300 139 67">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="30 300 139 67">
       <use xlinkHref={`${DestinationSVG}#${orbit}`} />
     </svg>
   </span>
