1d99557fb11e7f060fa2a0c9ec6c5b3ce6fabe1c
louisjc <louisjc@users.noreply.github.com>
2019-11-11 19:28:07 +0100

    orbitMap

--- src/scenes/PastLaunches/Destination.tsx
+++ src/scenes/PastLaunches/Destination.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react'
 import DestinationSVG from './destinations.svg'
 
-const Destination: FC<{ orbit: string }> = ({ orbit }) => (
-  <span className={`destination ${orbit}`} title={orbit}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="30 300 139 67">
-      <use xlinkHref={`${DestinationSVG}#${orbit}`} />
-    </svg>
-  </span>
-)
+function orbitMap(orbit: string) {
+  if (['VLEO'].includes(orbit)) {
+    return 'LEO'
+  }
+  return orbit
+}
+
+const Destination: FC<{ orbit: string }> = ({ orbit }) => {
+  orbit = orbitMap(orbit)
+  return (
+    <span className={`destination ${orbit}`} title={orbit}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="30 300 139 67">
+        <use xlinkHref={`${DestinationSVG}#${orbitMap(orbit)}`} />
+      </svg>
+    </span>
+  )
+}
 
 export default Destination
